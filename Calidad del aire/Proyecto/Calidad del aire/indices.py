4c463c95f274d81e0aa35ac9e449b640a325a365
Patatas1000 <juan.dsa99@gmail.com>
2025-04-12 17:35:51 -0500

    Prueba para poner en consideración el monóxido de carbono, pero analizarlo daña todo porque, básicamente causa la eliminación total de todo el dataframe, porque solo se mide en una estación

--- Calidad del aire/Proyecto/Calidad del aire/indices.py
+++ Calidad del aire/Proyecto/Calidad del aire/indices.py
@@ -15,19 +15,23 @@ def ICA(frame2):
     medias_dict = {}
 
     for columna in columnas:
-        medias = frame2.groupby([frame2.index.year,
-                                            frame2.index.month,
-                                            frame2.index.day])[columna].mean()
+        frame_filtrado = frame2[(frame2[columna] > 0) & (frame2[columna] < 1100)]
+        medias = frame_filtrado.groupby([frame_filtrado.index.year,
+                                            frame_filtrado.index.month,
+                                            frame_filtrado.index.day])[columna].mean()
+        # medias = frame2.groupby([frame2.index.year,
+        #                                     frame2.index.month,
+        #                                     frame2.index.day])[columna].mean()
         medias.index.names = ["Año", "Mes", "Día"]
         medias_df = medias.to_frame(name=columna)
-        medias_df.index = pd.to_datetime(frame2.groupby(
-            [frame2.index.year, frame2.index.month, frame2.index.day]
+        medias_df.index = pd.to_datetime(frame_filtrado.groupby(
+            [frame_filtrado.index.year, frame_filtrado.index.month, frame_filtrado.index.day]
         ).apply(lambda x: x.index[0]).values)
         medias_dict[columna] = medias_df
         # Convertir la serie a DataFrame y ajustar el índice
         medias_df = medias.to_frame(name=columna)
-        medias_df.index = pd.to_datetime(frame2.groupby(
-            [frame2.index.year, frame2.index.month, frame2.index.day]
+        medias_df.index = pd.to_datetime(frame_filtrado.groupby(
+            [frame_filtrado.index.year, frame_filtrado.index.month, frame_filtrado.index.day]
         ).apply(lambda x: x.index[0]).values)
         medias_dict[columna] = medias_df  # Almacenar el DataFrame en el diccionario
     frame3=pd.concat(medias_dict.values(), axis=1)
