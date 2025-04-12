d13b2dc4fa63b93e8f8092ee75f02b80e16bafd9
Patatas1000 <juan.dsa99@gmail.com>
2025-04-12 17:23:42 -0500

    Ya se realiza el tratamiento de obtener los valores promedios diarios sin eliminar datos, y se vuelve a crear el dataframe

--- Calidad del aire/Proyecto/Calidad del aire/indices.py
+++ Calidad del aire/Proyecto/Calidad del aire/indices.py
@@ -6,11 +6,37 @@ import glob
 import os
 from database_info import data
 
+path=r'Calidad del aire\Proyecto\Bases'
+frame2=data(path)
 
-def ICAcont():
+
+def ICA(frame2):
     columnas = ['pm25', 'pm10', 'no', 'no2', 'nox', 'ozono', 'so2']
+    medias_dict = {}
+
     for columna in columnas:
-        if columna=='pm25':
+        medias = frame2.groupby([frame2.index.year,
+                                            frame2.index.month,
+                                            frame2.index.day])[columna].mean()
+        medias.index.names = ["Año", "Mes", "Día"]
+        medias_df = medias.to_frame(name=columna)
+        medias_df.index = pd.to_datetime(frame2.groupby(
+            [frame2.index.year, frame2.index.month, frame2.index.day]
+        ).apply(lambda x: x.index[0]).values)
+        medias_dict[columna] = medias_df
+        # Convertir la serie a DataFrame y ajustar el índice
+        medias_df = medias.to_frame(name=columna)
+        medias_df.index = pd.to_datetime(frame2.groupby(
+            [frame2.index.year, frame2.index.month, frame2.index.day]
+        ).apply(lambda x: x.index[0]).values)
+        medias_dict[columna] = medias_df  # Almacenar el DataFrame en el diccionario
+    frame3=pd.concat(medias_dict.values(), axis=1)
+    # print(frame3.columns)
+    # print(frame3.index)
+
+
+
 
 
     # ICA = (((I_hi-I_lo)/(BP_hi-Bp_lo))*(C_cont-Bp_lo))-I_lo
+ICA(frame2)
