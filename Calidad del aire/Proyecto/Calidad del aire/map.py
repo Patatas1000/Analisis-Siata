e20ba452bcfad387e81b49a76c3b845115b1a222
Patatas1000 <juan.dsa99@gmail.com>
2025-04-17 20:31:57 -0500

    Pruebas con tkinterweb para ver si se puede poner el mapa directamente en la ventana

--- Calidad del aire/Proyecto/Calidad del aire/map.py
+++ Calidad del aire/Proyecto/Calidad del aire/map.py
@@ -1,3 +1,27 @@
 import folium
+import pandas as pd
+from database_info import coord
+import webbrowser
 
-map = folium.Map(location=[6.2442, -75.5812], zoom_start=12)
+path2=r'C:\Users\ivans\OneDrive\Desktop\Juan\Analisis-Siata\Calidad del aire\Proyecto\Estaciones'
+coordenadas = coord(path2)
+
+m=folium.Map(location=[6.25256, -75.56958], zoom_start=12)
+
+columnas = ['Estacion', 'Longitud', 'Latitud', 'Ciudad']
+
+Cities = ['Medellin', 'Medellín']
+
+fr={}
+
+for city in Cities:
+    coord2=coordenadas.loc[coordenadas['Ciudad'] == city, columnas]
+    fr[city] = coord2
+    for i, row in coord2.iterrows():
+        folium.Marker(
+            location=[row['Latitud'], row['Longitud']],
+            popup=row['Estacion'],
+            icon=folium.Icon(color='blue', icon='info-sign')
+        ).add_to(m)
+
+m.save('mapa.html')
---
+++ Calidad del aire/Proyecto/Calidad del aire/test.py
@@ -0,0 +1,52 @@
+# import tkinter as tk
+# from tkinter import ttk
+# import folium
+# import webbrowser
+
+# def mostrar_mapa():
+#     m = folium.Map(location=[40.7128, -74.0060], zoom_start=10)  # Ejemplo: Nueva York
+#     m.save('mi_mapa.html')
+#     webbrowser.open_new_tab('mi_mapa.html')
+
+# root = tk.Tk()
+# root.title("Integración de Mapa")
+
+# boton_mostrar = ttk.Button(root, text="Mostrar Mapa", command=mostrar_mapa)
+# boton_mostrar.pack(pady=20)
+
+# root.mainloop()
+
+# import tkinter as tk
+# from tkinter import ttk
+# from tkinterweb import HtmlFrame
+
+# root = tk.Tk()
+# root.title("Página Web en Tkinter")
+
+# frame_html = HtmlFrame(root)
+# frame_html.pack(fill="both", expand=True)
+# frame_html.load_file(r"C:\Users\ivans\OneDrive\Desktop\Juan\Analisis-Siata\mapa.html")
+
+# root.mainloop()
+
+import tkinter as tk
+from tkinterweb import HtmlFrame
+import folium
+
+# Crear un mapa con folium
+mapa = folium.Map(location=[6.5, -74.5], zoom_start=12)
+mapa.save("mapa2.html")  # Guardar el mapa en un archivo HTML
+
+# Crear ventana principal con tkinter
+root = tk.Tk()
+root.title("Mapa con tkinterweb")
+
+# Crear marco HTML
+frame = HtmlFrame(root)
+frame.pack(fill="both", expand=True)
+
+# Cargar el mapa HTML generado por folium
+frame.load_file(r"C:\Users\ivans\OneDrive\Desktop\Juan\Analisis-Siata\mapa2.html")  # Cargar el archivo HTML del mapa
+
+# Ejecutar la ventana
+root.mainloop()
